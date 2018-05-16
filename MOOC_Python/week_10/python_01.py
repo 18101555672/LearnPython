f9dd26f481d8bb6bd9e2014cf94b3d210ac35e2c
wuchenyang <sxx979528751@icloud.com>
2018-05-16 10:51:29 +0800

    MOOC College:The tenth week

---
+++ MOOC_Python/week_10/python_01.py
@@ -0,0 +1,27 @@
+# 从数据处理到人工智能
+# 数据处理：Numpy、Pandas、SciPy
+# 数据可视化：Matplotlib、Seaborn、Mayavi
+# 文本处理：PyFDF2、NLTK、python-docx
+# 机器学习：Scikit-learn、TensorFlow、MXNet
+
+# 从Web解析到网络空间
+# 爬虫：Requests、Scrapy、pyspider
+# 信息解析：Beautiful Soup、Re、Python-Goose
+# 网站开发：Django、Pyramid、Flask
+# 网络应用：WeRobot、aip、MyQR
+
+# 从人机交互到艺术设计
+# 图形用户界面：PyQt5、wxPython、PyGObject
+# 游戏开发：PyGame、Panda3D、cocos2d
+# 虚拟现实：VR Zero、pyovr、Vizard
+# 图形艺术：Quads、ascii_art、turtle
+
+
+
+
+
+
+
+
+
+
